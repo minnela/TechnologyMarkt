c5d88123be8456b60bf684220a8eae06d0a1fc83
Minel Aydın <49316771+minnela@users.noreply.github.com>
2021-01-24 15:21:14 +0300

    Initial Commit

---
+++ src/main/java/com/technology/markt/domain/Devices.java
@@ -0,0 +1,98 @@
+package com.technology.markt.domain;
+
+import javax.persistence.*;
+
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity
+@Table(name = "device_table")
+public abstract class Devices {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="brand")
+    private String brand;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="os")
+    private String os;
+
+    @Column(name="production_year")
+    private int productionYear;
+
+    @Column(name="color")
+    private String color;
+
+    @Column(name="screen_size")
+    private double screenSize;
+
+    @Column(name="comment")
+    private String comment;
+
+    public Devices() {
+    }
+
+    public Devices(String brand, String name) {
+        this.brand = brand;
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(double screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
